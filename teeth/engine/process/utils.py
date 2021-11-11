100cc8c2fb9d8151fed22304993192c1e061b9b3
Xypher <ammar.abu.yaman@gmail.com>
2021-11-11 17:31:32 +0200

    flipped and filled the central perfect images with black

--- teeth/engine/process/utils.py
+++ teeth/engine/process/utils.py
@@ -8,23 +8,23 @@ kernel = np.ones((5, 5), np.uint8)
 PERFECTS = {
     "buccal": {
         "premandibular": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/buccal-perfect-premandibular.jpg"), cv2.IMREAD_UNCHANGED),
-        "central": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/buccal-perfect-central.jpeg"), cv2.IMREAD_UNCHANGED)
+        "central": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/buccal-perfect-central.jpg"), cv2.IMREAD_UNCHANGED)
     },
     "distal": {
         "premandibular": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/distal-perfect-premandibular.jpg"), cv2.IMREAD_UNCHANGED),
-        "central": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/distal-perfect-central.jpeg"), cv2.IMREAD_UNCHANGED)
+        "central": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/distal-perfect-central.jpg"), cv2.IMREAD_UNCHANGED)
     },
     "mesial": {
         "premandibular": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/mesial-perfect-premandibular.jpg"), cv2.IMREAD_UNCHANGED),
-        "central":  cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/mesial-perfect-central.jpeg"), cv2.IMREAD_UNCHANGED)
+        "central":  cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/mesial-perfect-central.jpg"), cv2.IMREAD_UNCHANGED)
     },
     "lingual": {
         "premandibular":  cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/lingual-perfect-premandibular.jpeg"), cv2.IMREAD_UNCHANGED),
-        "central": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/lingual-perfect-central.jpeg"), cv2.IMREAD_UNCHANGED)
+        "central": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/lingual-perfect-central.jpg"), cv2.IMREAD_UNCHANGED)
     },
     "top_view": {
         "premandibular": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/top_view-perfect-premandibular.jpg"), cv2.IMREAD_UNCHANGED),
-        "central": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/top_view-perfect-central.jpeg"), cv2.IMREAD_UNCHANGED)
+        "central": cv2.imread(os.path.join(settings.STATIC_ROOT, "engine/top_view-perfect-central.jpg"), cv2.IMREAD_UNCHANGED)
     }
 }
 

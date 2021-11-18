1da28e4e01a48e408dc233a25878c752995a86f3
YazanAssaf <93004160+YazanAssaf@users.noreply.github.com>
2021-11-18 10:39:41 +0200

    Update mesial.py

--- teeth/engine/process/mesial.py
+++ teeth/engine/process/mesial.py
@@ -112,12 +112,13 @@ def mesial(img, type):
     coef_x = w_t / w_p
     cnt_perfect[:, :, 0] = cnt_perfect[:, :, 0] * coef_x
     cnt_perfect[:, :, 1] = cnt_perfect[:, :, 1] * coef_y
+    x_p, y_p, w_p, h_p = cv2.boundingRect(cnt_perfect)
     cv2.drawContours(blank_image1, [cnt_tooth], -1, (255, 255, 255), -1)
-    cv2.drawContours(blank_image2, [cnt_perfect], -1, (255, 255, 255), -1, offset=(x_t, y_t))
+    cv2.drawContours(blank_image2, [cnt_perfect], -1, (255, 255, 255), -1, offset=(x_t-x_p, y_t-y_p))
     shape_match = shapeMatch(blank_image1, blank_image2,cnt_perfect)
     shape_img = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
     cv2.drawContours(shape_img, [cnt_tooth], -1, (255, 255, 255), -1)
-    cv2.drawContours(shape_img, [cnt_perfect], -1, (0, 0, 139), 10, offset=(x_t , y_t))
+    cv2.drawContours(shape_img, [cnt_perfect], -1, (0, 0, 139), 10, offset=(x_t-x_p , y_t-y_p))
     a10 = f"shape matching= {shape_match}"
     arrayofString.append(a10)
 

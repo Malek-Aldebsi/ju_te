95245f84909cc097e36798c2c9a4c833282be32a
Suhaib AlTamimi <shbzt01@gmail.com>
2022-09-28 20:01:59 +0300

    add CORS to settings.py

--- teeth/settings.py
+++ teeth/settings.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:3000',
-  'https://shbzt01.github.io/'
+  'https://shbzt01.github.io'
 )
 
 django_on_heroku.settings(locals())
